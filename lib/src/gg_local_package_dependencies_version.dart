161dd1d0b6e243709c9a6b314dc268826780e093
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-08-08 07:37:50 +0200

    Allow to pass custom options to exec of dir commands.
    * #gg: changed references to path
    
    * Allow to pass custom options to exec of dir commands.
    
    * #gg: Add .gg/gg.json check results
    
    * #gg: dart pub upgrade --major-versions --tighten
    
    * #gg: Add .gg/gg.json check results
    
    * #gg: changed references to pub.dev
    
    * #gg: Finish development of version 3.0.0

--- lib/src/gg_local_package_dependencies_version.dart
+++ lib/src/gg_local_package_dependencies_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_local_package_dependencies` package.
-const String ggLocalPackageDependenciesVersion = '2.1.0';
+const String ggLocalPackageDependenciesVersion = '3.0.0';
